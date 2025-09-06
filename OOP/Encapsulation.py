4105a1ee0ccbd259e44b4cfcfe9a08e0f989f6f7
Chaitanya07789 <chaitanyalondhe730@gmail.com>
2025-09-06 22:25:30 +0530

    encapsulation

---
+++ OOP/Encapsulation.py
@@ -0,0 +1,22 @@
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance # private variable
+
+    def deposite(self , amount):
+        self.__balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("Insufficient balance")
+
+    def get_balance(self):
+        return self.__balance
+    
+
+acc = BankAccount(1000)
+acc.deposite(500)
+print(acc.get_balance()) # 1500
+acc.__balance = 0  # cannot modify directly encupsulation prevents this
+print(acc.get_balance()) # 1500
